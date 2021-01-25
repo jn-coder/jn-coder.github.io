7319daf70e24e64386cfe45df4b28cc58464cc4a
John Nelson <john@mikelopezuniversity.ml>
2021-01-25 00:34:29 -0600

    consolidate forms into 1 and re-write scrips.js UI logic

--- calculator-app/js/scripts.js
+++ calculator-app/js/scripts.js
@@ -18,36 +18,21 @@ function divide(number1, number2) {
 
 // User interface logic:
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-   event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
   });
-
-  $("form#sub").submit(function(event) {
-    event.preventDefault();
-     const number1 = parseInt($("#sub1").val());
-     const number2 = parseInt($("#sub2").val());
-     const result = subtract(number1, number2);
-     $("#output").text(result);
-   });
-  
-   $("form#mul").submit(function(event) {
-    event.preventDefault();
-     const number1 = parseInt($("#mul1").val());
-     const number2 = parseInt($("#mul2").val());
-     const result = multiply(number1, number2);
-     $("#output").text(result);
-   });
-
-   $("form#div").submit(function(event) {
-    event.preventDefault();
-     const number1 = parseInt($("#div1").val());
-     const number2 = parseInt($("#div2").val());
-     const result = divide(number1, number2);
-     $("#output").text(result);
-   });
-
 });
