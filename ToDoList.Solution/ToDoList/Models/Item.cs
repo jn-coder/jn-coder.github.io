831d288f2ccea8cfb1bc3bae4b7663f67ad9eb98
John Nelson <jrnelson654@gmail.com>
2021-02-03 03:58:13 -0600

    add more tests to ToDoList project

--- ToDoList.Solution/ToDoList/Models/Item.cs
+++ ToDoList.Solution/ToDoList/Models/Item.cs
@@ -1,9 +1,21 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
+    public string Description { get; set; }
+		private static List<Item> _instances = new List<Item> {};
 
-    // properties, methods, etc. will go here.
+    public Item(string description)
+    {
+      Description = description;
+			_instances.Add(this);
+		}
+		public static List<Item> GetAll()
+		{
+  		return _instances;
+		}
 
   }
 }
